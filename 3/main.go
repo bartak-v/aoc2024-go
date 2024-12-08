586aedba087c8a549634d4f18ba134c53ea75f32
Vít Barták <46031140+bartak-v@users.noreply.github.com>
2024-12-08 17:47:46 +0100

    day 3 part 1

--- 3/main.go
+++ 3/main.go
@@ -6,9 +6,11 @@ import (
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
 func main() {
+	var final_count int = 0
 	file, err := os.ReadFile("input")
 	if err != nil {
 		log.Fatal(err)
@@ -16,13 +18,16 @@ func main() {
 	str := string(file) // convert content to a 'string'
 	re := regexp.MustCompile(`mul\((\d+),(\d+)\)`)
 	matches := re.FindAllString(str, -1)
-	result := make([][]int, 0)
-
-	// Convert matches to integers and append to result
+	re = regexp.MustCompile(`(\d+),(\d+)`)
 	for _, match := range matches {
-		num1, _ := strconv.Atoi(string(match[1]))
-		num2, _ := strconv.Atoi(string(match[2]))
-		result = append(result, []int{num1, num2})
+		// extract numbers
+		submatch := re.FindAllString(match, -1)
+		for _, m := range submatch {
+			stringSlice := strings.Split(m, ",")
+			m1, _ := strconv.Atoi(string(stringSlice[0]))
+			m2, _ := strconv.Atoi(string(stringSlice[1]))
+			final_count += m1 * m2
+		}
 	}
-	fmt.Printf("%q\n",result)
+	fmt.Print(final_count)
 }
